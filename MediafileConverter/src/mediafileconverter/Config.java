499804bffb9dd3bec3187bca85942f108e3cf8b0
zinthanmm <zinthanmm@gmail.com>
2020-07-18 02:13:32 +0530

    configurations added for flv and mkv file conversion

--- MediafileConverter/src/mediafileconverter/Config.java
+++ MediafileConverter/src/mediafileconverter/Config.java
@@ -11,20 +11,80 @@ public class Config {
         
         System.err.println("Converting Started........(MP4 To MP3)");
         
-        Encoder forMp3 = new Encoder();
-        EncodingAttributes specificationsForMp3 = new EncodingAttributes();
-        specificationsForMp3.setFormat("mp3");
-        AudioAttributes audioForMp3 = new AudioAttributes();
-        audioForMp3.setVolume(256);
-        audioForMp3.setCodec("mp2");
-        specificationsForMp3.setAudioAttributes(audioForMp3);
+        Encoder encode = new Encoder();
+        EncodingAttributes attr = new EncodingAttributes();
+        attr.setFormat("mp3");
+        AudioAttributes audio = new AudioAttributes();
+        audio.setVolume(256);
+        audio.setCodec("mp2");
+        attr.setAudioAttributes(audio);
 
         try {
-            forMp3.encode(source, output, specificationsForMp3);
+            encode.encode(source, output, attr);
             System.err.println("Converting Completed........(MP4 To MP3)");
-        } catch (EncoderException ex) {
-            System.err.println(ex.toString());
+        } catch (EncoderException e) {
+            System.err.println(e.toString());
         }
     }
+    
+    //mp4Toflv
+    public static void mp4Toflv(File source, File output) {
+        
+        System.err.println("Converting Started........(MP4 To FLV)");
+        
+        Encoder encode = new Encoder();
+
+        EncodingAttributes attr = new EncodingAttributes();
+        attr.setFormat("flv");
+        AudioAttributes audio = new AudioAttributes();
+        audio.setCodec("libmp3lame");
+        audio.setBitRate(new Integer(64000));
+        audio.setChannels(new Integer(1));
+        audio.setSamplingRate(new Integer(22050));
+        VideoAttributes video = new VideoAttributes();
+        video.setCodec("flv");
+        video.setBitRate(new Integer(160000));
+        video.setFrameRate(new Integer(15));
+        attr.setAudioAttributes(audio);
+        attr.setVideoAttributes(video);
+
+        try {
+            encode.encode(source, output, attr);
+            System.err.println("Converting Completed........(MP4 To FLV)");
+        } catch (EncoderException e) {
+            System.err.println(e.toString());
+        }
+    }
+    
+    //mp4Tomkv
+    public static void mp4Tomkv(File source, File output) {
+        
+        System.err.println("Converting Started........(MP4 To MKV)");
+        
+        Encoder encode = new Encoder();
+
+        EncodingAttributes attr = new EncodingAttributes();
+        attr.setFormat("matroska");
+        AudioAttributes audio = new AudioAttributes();
+        audio.setCodec("libmp3lame");
+        audio.setBitRate(new Integer(64000));
+        audio.setChannels(new Integer(1));
+        audio.setSamplingRate(new Integer(22050));
+        VideoAttributes video = new VideoAttributes();
+        video.setCodec("mpeg4");
+        video.setBitRate(new Integer(160000));
+        video.setFrameRate(new Integer(15));
+        attr.setAudioAttributes(audio);
+        attr.setVideoAttributes(video);
+
+        try {
+            encode.encode(source, output, attr);
+            System.err.println("Converting Completed........(MP4 To MKV)");
+        } catch (EncoderException e) {
+            System.err.println(e.toString());
+        }
+    }
+    
+    
     
 }
--- MediafileConverter/src/mediafileconverter/main.java
+++ MediafileConverter/src/mediafileconverter/main.java
@@ -10,23 +10,83 @@ public class main {
 //        File Video = new File("C:\\Users\\ZNR\\Desktop\\MediaConverter\\media\\mp4Tomp3\\test1.mp4");
 //        mp4Tomp3.ConvertTomp3(Video);
 
-
-    //mp4Tomp3
-    File[] files = new File("C:\\Users\\ZNR\\Desktop\\MediaConverter\\media\\mp4Tomp3").listFiles();
-
-    File path = new File("C:\\Users\\ZNR\\Desktop\\MediaConverter\\media-converted\\mp4Tomp3-converted");
-
-      for (File file : files) {
-        String[] nameFile = file.getAbsolutePath().split("\\\\");
-        String[] removeExtension = nameFile[nameFile.length - 1].split("\\.");
-
-        File target = new File(path.getAbsolutePath() + "\\" + removeExtension[0] + ".mp3");
-
-        Config.mp4Tomp3(file, target);
-
-        file.deleteOnExit();
-      }
+        mp4Tomp3();
+        mp4Toflv();
+        mp4Tomkv();
+    
         
     }
+    
+    
+    //mp4Tomp3
+    public static void mp4Tomp3(){
+        
+        File[] files = new File("C:\\Users\\ZNR\\Desktop\\MediaConverter\\media\\mp4Tomp3").listFiles();
+        File path = new File("C:\\Users\\ZNR\\Desktop\\MediaConverter\\media-converted\\mp4Tomp3-converted");
+
+        for (File file : files) {
+            String[] nameFile = file.getAbsolutePath().split("\\\\");
+            String[] removeExtension = nameFile[nameFile.length - 1].split("\\.");
+
+            File target = new File(path.getAbsolutePath() + "\\" + removeExtension[0] + ".mp3");
+
+            Config.mp4Tomp3(file, target);
+
+            file.deleteOnExit();
+            
+      }
+    }
+    
+    //mp4TOflv
+    public static void mp4Toflv(){
+        
+        File[] files = new File("C:\\Users\\ZNR\\Desktop\\MediaConverter\\media\\mp4Toflv").listFiles();
+        File path = new File("C:\\Users\\ZNR\\Desktop\\MediaConverter\\media-converted\\mp4Toflv-converted");
+
+        for (File file : files) {
+            String[] nameFile = file.getAbsolutePath().split("\\\\");
+            String[] removeExtension = nameFile[nameFile.length - 1].split("\\.");
+
+            File target = new File(path.getAbsolutePath() + "\\" + removeExtension[0] + ".flv");
+
+            Config.mp4Toflv(file, target);
+
+            file.deleteOnExit();
+            
+      }
+    }
+    
+    //mp4TOmkv
+    public static void mp4Tomkv(){
+        
+        File[] files = new File("C:\\Users\\ZNR\\Desktop\\MediaConverter\\media\\mp4Tomkv").listFiles();
+        File path = new File("C:\\Users\\ZNR\\Desktop\\MediaConverter\\media-converted\\mp4Tomkv-converted");
+
+        for (File file : files) {
+            String[] nameFile = file.getAbsolutePath().split("\\\\");
+            String[] removeExtension = nameFile[nameFile.length - 1].split("\\.");
+
+            File target = new File(path.getAbsolutePath() + "\\" + removeExtension[0] + ".mkv");
+
+            Config.mp4Tomkv(file, target);
+
+            file.deleteOnExit();
+            
+      }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
